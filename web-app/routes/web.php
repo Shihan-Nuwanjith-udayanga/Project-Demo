cadc252ec6d947fe593f3f9b35975dff0b624dd3
Shihan-Nuwanjith-udayanga <shihannuwanjith@gmail.com>
2025-09-07 22:23:55 +0530

    Update Home Route

--- web-app/routes/web.php
+++ web-app/routes/web.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home',function(){
-    return 123;
+Route::get('/',function(){
+    return view('Pages.home');
 });
 
 Route::get('/dashboard', function () {
