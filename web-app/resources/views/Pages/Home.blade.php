f82baa0dd440e797d4e6ad3dcc60c98794232f3e
Shihan-Nuwanjith-udayanga <shihannuwanjith@gmail.com>
2025-09-12 22:41:28 +0530

    remove pages folder

--- web-app/resources/views/Pages/Home.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>Home Page</h1>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ipsa deleniti repudiandae sint perspiciatis et voluptas nemo. Deserunt quis cupiditate commodi dicta quae laborum? Ducimus iusto repudiandae voluptatem quisquam commodi.</p>
